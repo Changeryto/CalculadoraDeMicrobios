e07247571fd6be8d091582a743243a3be354f219
Rubén Téllez Gerardo <79559187+Changeryto@users.noreply.github.com>
2024-05-31 15:47:17 -0600

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/ruben/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/ruben/Documentos/Apps_propias/calculadora_de_microbios" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.1+2" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 2 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/ruben/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/ruben/Documentos/Apps_propias/calculadora_de_microbios"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC83NGQxNjYyN2I5NDBiYjE1ZTUwODkxZjgyY2FkNmMzZTM0NjViZDZkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/ruben/Documentos/Apps_propias/calculadora_de_microbios/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/ruben/Documentos/Apps_propias/calculadora_de_microbios/lib/main.dart"
+)
